a9c8cff70bb5641385ac330c974d8906bfbdd7ee
ZiDiYao <zidiyaocanada@outlook.com>
2025-05-24 11:21:48 +0900

    init

---
+++ src/test/java/com/chatnest/chatnestuserservice/ChatnestUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatnest.chatnestuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatnestUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
