9f8ecc65496f907024efb02a0c9b3da941039fa0
ZiDiYao <zidiyaocanada@outlook.com>
2025-05-25 23:33:50 +0900

    update config

---
+++ src/main/java/com/chatnest/chatnestuserservice/config/JwtInterceptor.java
@@ -0,0 +1,21 @@
+package com.chatnest.chatnestuserservice.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class JwtInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+
+        return true;
+    }
+
+
+
+
+
+
+}
---
+++ src/main/java/com/chatnest/chatnestuserservice/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package com.chatnest.chatnestuserservice.config;
+
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ *
+ * JwtInterceptor 是一个“请求拦截器”，
+ * 它在请求到达你的 Controller 之前，先检查请求里有没有合法的 JWT Token，
+ * 并提取出用户身份信息（比如 userId）供你后续使用。
+ */
+public class WebMvcConfig implements WebMvcConfigurer {
+
+
+}
